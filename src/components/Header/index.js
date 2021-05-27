876ed73aa845c8d0adcd023ae57a34ed11656a55
Juan Celaya <jcelayardz@gmail.com>
2021-05-27 20:04:03 +0200

    Refactor code, delete navbar component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,17 @@
-import { Navbar } from '../Navbar'
-import { StyledHeader } from './styles'
+import { StyledHeader, Navbar, Title, ThemeBtn } from './styles'
 
 export const Header = ({handleToggleTheme}) => {
     return(
         <StyledHeader>
-            <Navbar
-                handleToggleTheme={handleToggleTheme}
-            />
+            <Navbar>
+                <Title>Where in the world?</Title>
+                <div>
+                     <i className="fas fa-moon"></i>
+                    <ThemeBtn onClick={handleToggleTheme}>
+                     Dark Mode
+                    </ThemeBtn>
+                </div>
+            </Navbar>
         </StyledHeader>
     )
 }
